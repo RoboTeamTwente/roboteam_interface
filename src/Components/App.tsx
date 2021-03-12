72c7c7061e2b64afa51799d3f335274ebf0017a5
nathaliekamp98 <73992306+nathaliekamp98@users.noreply.github.com>
2021-03-12 16:44:27 +0100

    layout rewritten to materials-ui grid

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -4,7 +4,6 @@ import Field from "./Field/Field";
 import SettingsWidget from "./Settings/SettingsWidget";
 import "../Styles/main.css";
 import {
-  createMuiTheme,
   CssBaseline,
   responsiveFontSizes,
   ThemeProvider,
@@ -28,10 +27,14 @@ type AppState = {
 }
 
 class App extends React.Component<{}, AppState> {
+
     private readonly defaultStateData: ModuleState = {
         systemState: {state: undefined, uiSettings: {uiValues: {}}},
         handshakes: []
     };
+
+    readonly root = "flexGrow: 1";
+    readonly paper = 'textAlign: "center"';
 
     constructor(props: any) {
         super(props);
@@ -50,6 +53,15 @@ class App extends React.Component<{}, AppState> {
 
     componentDidMount() {
         const [host, port] = getStartingPortHostnameCombination();
+//materailsui imports
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { createMuiTheme, makeStyles } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
+
+const useStyles = makeStyles((theme) => ({
+  paper: { backgroundColor: "blue" },
+}));
 
         this.installWebsocket(new WebSocket(hostnamePortPairToWSURL(host, port, "")));
 
@@ -63,38 +75,67 @@ class App extends React.Component<{}, AppState> {
     render() {
         return (
             <body>
-            <div className="row">
-                <div className="column field">
-                    <Field transformation={0}></Field>
-                    <div className="wrapperControls">
-                        <div className="row">
-                            <div className="column">
-                                <p className="grey">Robots on our Team: </p>
-                            </div>
-                            <div className="column">
-                                <p> 8 </p>
-                            </div>
-                            <div className="column">
-                                <p className="grey">Playing as:</p>
-                            </div>
-                            <div className="column">
-                                <p>yellow</p>
-                            </div>
-                            <div className="column">
-                                <button className="button">switch sides</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="column settings">
-                    <SettingsWidget/>
-                </div>
-            </div>
-            <div className="wrapperControls">
-                <button>Replay</button>
-            </div>
-            <div className="roboteamLogoTopDiv">
-                <img className="roboteamLogoTop" src={logo} alt="Logo"/>
+            <div className={this.root}>
+              <Grid container spacing={3}>
+                <Grid item xs={7} container>
+                  <Grid item xs={12}>
+                    <Paper className={"column"}>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                      <p>field</p>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Paper className={"column"}>
+                      <p>controls</p>
+                      <p>controls</p>
+                      <p>controls</p>
+                      <p>controls</p>
+                    </Paper>
+                  </Grid>
+                </Grid>
+                <Grid item xs={5} container>
+                  <Grid item xs={12}>
+                    <Paper className={"column"}>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                      <p>info</p>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Paper className={"column"}>
+                      <img className="roboteamLogo" src={logo} alt="Logo" />
+                    </Paper>
+                  </Grid>
+                </Grid>
+              </Grid>
             </div>
             </body>);
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,37 @@
-import App from './Components/App';
-import ReactDOM from 'react-dom'
+import App from "./Components/App";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import red from "@material-ui/core/colors/red";
+import { createMuiTheme } from "@material-ui/core/styles";
 import protobuf from "protobufjs";
 import Long from "long";
 
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+});
 protobuf.util.Long = Long;
 protobuf.configure();
 
 ReactDOM.render(
-    <App/>,
-    document.getElementById('root') as HTMLElement
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root") as HTMLElement
 );
 // TODO: Add service worker(s)?
